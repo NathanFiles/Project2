5bb268abcfeb2fe33f02ac6fbdb9620916202682
Nathan Files <en.files@outlook.com>
2019-10-02 13:35:31 -0400

    changes to home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './app/modules/threads/components/components.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent
   ],
   imports: [
     BrowserModule,
