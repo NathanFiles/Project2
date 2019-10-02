a70d624d3820a17898a4f6dd484ece9b0ff7468c
krishnakafley <krishnakafleyp@gmail.com>
2019-10-02 12:38:25 -0400

    backend springboot template

---
+++ ImageBoardBackend/src/main/java/com/imageboard/ImageBoardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.imageboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageBoardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageBoardBackendApplication.class, args);
+	}
+
+}
