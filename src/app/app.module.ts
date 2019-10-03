956ac6a00b2f9377a8a1a591654b0a607b87bfbf
Shigal Hammoud <shigalhammoud@gmail.com>
2019-10-03 08:51:25 -0400

    made a new branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ThreadsComponent } from './component/threads/threads.component';
+import { PostComponent } from './component/post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ThreadsComponent
+    ThreadsComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
