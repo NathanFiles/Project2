0b1056a9b26ae233a889a00eb08462ea9c7c6517
Shigal Hammoud <shigalhammoud@gmail.com>
2019-10-12 09:55:20 -0400

    fixed the host

--- src/app/service/post.service.ts
+++ src/app/service/post.service.ts
@@ -32,8 +32,7 @@ export class PostService {
   }
 
   threadnum = -1;
-  // host :string = "http://ec2-3-19-227-34.us-east-2.compute.amazonaws.com:8080/";
-  host :string = "http://ec2-18-218-37-90.us-east-2.compute.amazonaws.com:8080"
+  host :string = "http://ec2-3-19-227-34.us-east-2.compute.amazonaws.com:8080/";
   // host :string = "http://localhost:8080";
 
   getPost(p_id :number) :Observable<post> {
